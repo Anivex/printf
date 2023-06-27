aaae95a248ae468078e2c095fe433813b12012f1
Anivex <60237060+Anivex@users.noreply.github.com>
2023-06-27 20:34:33 +0100

    Update _puts.c

--- _puts.c
+++ _puts.c
@@ -1,35 +1,114 @@
 #include "main.h"
 
-/**
- * _putchar - print character.
- *
- * @c: char.
- *
- * Return: 1.
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
 /**
- * _puts - prints string.
+ * _strlen - count length of format.
  *
- * @s: string.
+ * @s: format.
  *
- * Return: number of character.
+ * Return: length of format.
  */
-int _puts(char *s)
+
+int _strlen(const char *s)
 {
 	int i = 0;
 
 	if (!s)
-	{
-		_puts("(null)");
-		return (6);
-	}
+		return (-1);
 	while (s[i])
-		_putchar(s[i++]);
+		i++;
+
 	return (i);
+}
+
+int specifier(char c, va_list ap)
+{
+	int num = 0;
+
+	switch (c)
+	{
+		case 'c':
+			num += _putchar(va_arg(ap, int));
+			break;
+		case 's':
+			num += _puts(va_arg(ap, char *));
+			break;
+		case '%':
+			num += _putchar('%');
+			break;
+		case 'i':
+		case 'd':
+			num += _itoa(va_arg(ap, int));
+			break;
+		default:
+			num += _putchar('%');
+			num += _putchar(c);
+			break;
+	}
+
+	return (num);
+}
+
+int _itoa(int n)
+{
+	char *s, c;
+	int i = 0, rem, count = 0;
+
+	s = malloc(sizeof(char) * 11);
+	if (s == NULL)
+	{
+		return (0);
+	}
+	if (n < 0)
+	{
+		s[0] = '-';
+		n = n * (-1);
+	}
+	while (n != 0)
+	{
+		rem = n % 10;
+		n = n / 10;
+		c = '0' + rem;
+		s[10 - i] = c;
+		i++;
+		count++;
+	}
+	for (i = 0 ; i < 11; i++)
+        _putchar(s[i]);
+
+	return (count);
+}
+/**
+ * _printf - is the C language function to do format-ted printing.
+ *
+ * @format: string.
+ *
+ * Return: number of character printed.
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int i, len, numc = 0;
+
+	len = _strlen(format);
+	if (len == -1)
+		return (-1);
+	va_start(ap, format);
+	for (i = 0; i < len; i++)
+	{
+
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i])
+				numc += specifier(format[i], ap);
+			else
+				return (-1);
+		}
+		else
+			numc += _putchar(format[i]);
+	}
+	va_end(ap);
+	return (numc);
 }
