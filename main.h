d423f936d809139dda702f162ecd1c9a0994a88b
Anivex <60237060+Anivex@users.noreply.github.com>
2023-06-27 13:27:56 +0100

    Create main.h

---
+++ main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+
+
+int _puts(char *s);
+int _putchar(char c);
+int _strlen(const char *s);
+int _printf(const char *format, ...);
